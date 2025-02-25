a0f0f97163d0d071d7a94da7306317864612eb33
Sssrajputt <149288600+Sssrajputt@users.noreply.github.com>
2025-02-25 11:40:31 +0530

    [Shubham Rajput] Added : Hello-Messaging-App-UC1

---
+++ CG-Spring-Hello-Messaging-App-UC1/HelloMessagingApplication/src/main/java/com/bridgelabz/HelloMessagingApplication/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.bridgelabz.HelloMessagingApplication.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String getHello() {
+        return "Hello from BridgeLabz (GET)";
+    }
+
+    @PostMapping
+    public String postHello() {
+        return "Hello from BridgeLabz (POST)";
+    }
+
+    @PutMapping
+    public String putHello() {
+        return "Hello from BridgeLabz (PUT)";
+    }
+
+    @DeleteMapping
+    public String deleteHello() {
+        return "Hello from BridgeLabz (DELETE)";
+    }
+}
---
+++ CG-Spring-Hello-Messaging-App-UC1/HelloMessagingApplication/src/test/java/com/bridgelabz/HelloMessagingApplication/HelloMessagingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.HelloMessagingApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMessagingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
